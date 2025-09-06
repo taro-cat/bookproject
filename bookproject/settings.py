7bf801b5dcffc233383f45143a2ae169fcafad3f
taro-cat <tarotarochi@gmail.com>
2025-09-06 13:52:17 +0900

    cloudinary

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -135,8 +135,7 @@ else:
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-if not DEBUG:
-    cloudinary.config( 
+cloudinary.config( 
         cloud_name = config('CLOUDINARY_CLOUD_NAME'), 
         api_key = config('CLOUDINARY_API_KEY'), 
         api_secret = config('CLOUDINARY_API_SECRET') 
